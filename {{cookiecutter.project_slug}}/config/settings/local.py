b075836df79c121700587cbf43b2a8d5eb8f3404
Cuong Nguyen <cuongnb14@gmail.com>
2018-05-08 17:59:41 +0700

    config logging for project

--- {{cookiecutter.project_slug}}/config/settings/local.py
+++ {{cookiecutter.project_slug}}/config/settings/local.py
@@ -68,5 +68,85 @@ TEST_RUNNER = 'django.test.runner.DiscoverRunner'
 CELERY_ALWAYS_EAGER = True
 ########## END CELERY
 
+
+# LOGGING CONFIG
+# ------------------------------------------------------------------------------
+SLACK_LOG_API_KEY = env('SLACK_LOG_API_KEY',
+                        default='xoxb-32068700358')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(module)s %(message)s'
+        },
+        'simple-time': {
+            'format': '%(levelname)s [%(asctime)s+0000] %(module)s %(message)s'
+        }
+    },
+    'handlers': {
+        '{{cookiecutter.project_slug}}-file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'filename': 'logs/{{cookiecutter.project_slug}}.log',
+            'formatter': 'verbose',
+            'when': 'D',
+            'backupCount': 3,
+        },
+        'django-file': {
+            'level': 'WARNING',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'filename': 'logs/django.log',
+            'formatter': 'verbose',
+            'when': 'D',
+            'backupCount': 3,
+        },
+        'slack-error': {
+            'level': 'ERROR',
+            'api_key': SLACK_LOG_API_KEY,
+            'class': 'slacker_log_handler.SlackerLogHandler',
+            'channel': '#{{cookiecutter.project_slug}}',
+            'icon_emoji': ':rocket:',
+            'username': '{{cookiecutter.project_slug}} Error',
+            'formatter': 'verbose',
+        },
+        'slack-info': {
+            'level': 'INFO',
+            'api_key': SLACK_LOG_API_KEY,
+            'class': 'slacker_log_handler.SlackerLogHandler',
+            'channel': '#{{cookiecutter.project_slug}}',
+            'icon_emoji': ':small_airplane:',
+            'username': '{{cookiecutter.project_slug}} Info',
+            'formatter': 'verbose',
+        },
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['slack-error', 'django-file', 'console'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        # '{{cookiecutter.project_slug}}': {
+        #     'handlers': ['slack-error', '{{cookiecutter.project_slug}}-file'],
+        #     'level': 'ERROR',
+        #     'propagate': True,
+        # },
+        # 'slack.info': {
+        #     'handlers': ['slack-info'],
+        #     'level': 'INFO',
+        #     'propagate': True,
+        # }
+    },
+}
+
 # Your local stuff: Below this line define 3rd party library settings
 # ------------------------------------------------------------------------------
